875092c17a129d764047602bdb01ef3d94a45df8
PhucPhan <phucphan.pukaco@gmail.com>
2018-09-28 16:46:39 +0700

    page editprofile

---
+++ client/src/app/components/editprof/editprof.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editprof',
+  templateUrl: './editprof.component.html',
+  styleUrls: ['./editprof.component.css']
+})
+export class EditprofComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
