6a3d3f5ed34cb8386ff9be75192235de982420fe
DESKTOP-BAKG7GD\PhucP <phucphan6666@gmail.com>
2018-10-04 22:59:59 +0700

    router

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms'
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -11,6 +12,30 @@ import { EditprofComponent } from './components/editprof/editprof.component';
 import { ChatComponent } from './components/chat/chat.component';
 import { CreateEventComponent } from './components/create-event/create-event.component';
 import { FriendlistComponent } from './components/friendlist/friendlist.component';
+import { NewfeedsComponent } from './components/newfeeds/newfeeds.component';
+
+const appRoutes : Routes = [
+  {
+    path: '',
+    component: NewfeedsComponent
+  },
+  {
+    path: 'home',
+    component: NewfeedsComponent
+  },
+  {
+    path: 'friend',
+    component: FriendlistComponent
+  },
+  {
+    path: 'chat',
+    component: ChatComponent
+  },
+  {
+    path: 'createEvent',
+    component: CreateEventComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -21,12 +46,14 @@ import { FriendlistComponent } from './components/friendlist/friendlist.componen
     EditprofComponent,
     ChatComponent,
     CreateEventComponent,
-    FriendlistComponent
+    FriendlistComponent,
+    NewfeedsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
